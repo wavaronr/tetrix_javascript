c57d3663c0d29c7be2fbc890c27740011fd2b4e1
wavaronr <alexney1@hotmail.com>
2024-03-16 23:35:03 -0500

    create App

---
+++ TetrixJavascript/main.js
@@ -0,0 +1,156 @@
+import "./style.css";
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+const BLOCK_SIZE = 20;
+const BOARD_WIDTH = 14;
+const BOARD_HEIGHT = 30;
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH;
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT;
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+];
+
+const piece = {
+  position: { x: 5, y: 5 },
+  shape: [
+    [1, 1],
+    [1, 1],
+  ],
+};
+
+let dropCounter = 0;
+let lastTime = 0;
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+
+  if (dropCounter > 1000) {
+    piece.position.y++;
+    dropCounter = 0;
+  }
+  draw();
+  window.requestAnimationFrame(update);
+}
+
+function draw() {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        context.fillStyle = "yellow";
+        context.fillRect(x, y, 1, 1);
+      }
+    });
+  });
+
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value) {
+        context.fillStyle = "red";
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1);
+      }
+    });
+  });
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    piece.position.x--;
+    if (checkcollision()) {
+      piece.position.x++;
+    }
+  }
+
+  if (event.key === "ArrowRight") {
+    piece.position.x++;
+    if (checkcollision()) {
+      piece.position.x--;
+    }
+  }
+  if (event.key === "ArrowDown") {
+    piece.position.y++;
+    if (checkcollision()) {
+      piece.position.y--;
+      solidifypiece();
+      removeRows();
+    }
+  }
+});
+
+function checkcollision() {
+  return piece.shape.find((row, y) => {
+    return row.find((value, x) => {
+      return (
+        value !== 0 && board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      );
+    });
+  });
+}
+
+function solidifypiece() {
+  piece.shape.forEach((row, x) => {
+    row.forEach((value, y) => {
+      if (value === 1) {
+        board[y + piece.position.y][x + piece.position.x] = 1;
+      }
+    });
+  });
+  piece.position.x = 0;
+  piece.position.y = 0;
+}
+
+function removeRows() {
+  const rowsToRemove = [];
+
+  board.forEach((row, y) => {
+    if (row.every((value) => value === 1)) {
+      rowsToRemove.push(y);
+    }
+  });
+  rowsToRemove.forEach((y) => {
+    board.splice(y, 1);
+    const newRow = Array(BOARD_HEIGHT).fill(0);
+    board.unshift(newRow);
+  });
+}
+
+update();
